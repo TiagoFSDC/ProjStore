e8a2d09db836e2c3c8a28083914ec717a08e075d
TiagoFSDC <tiagofsdc@gmail.com>
2023-04-18 23:31:51 -0300

    Add project files.

---
+++ Controllers/StoreController.cs
@@ -0,0 +1,25 @@
+﻿using Models;
+using Services;
+
+namespace Controllers
+{
+    public class StoreController
+    {
+        private StoreServices storeService;
+
+        public StoreController()
+        {
+            storeService = new StoreServices();
+        }
+
+        public bool Insert(Store store)
+        {
+            return storeService.Insert(store);
+        }
+
+        public List<Store> GetAll() 
+        {
+            return storeService.GetAll();   
+        }
+    }
+}
---
+++ ProjStore/Program.cs
@@ -0,0 +1,22 @@
+﻿using Controllers;
+using Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto Loja");
+
+        var data = new Store()
+        {
+            Description = "Teste",
+            Address = "Rua do alem"
+        };
+
+        string returninformation = (new StoreController().Insert(data) ? "Registro Inserido" : "Erro");
+
+        Console.WriteLine(returninformation);
+
+        new StoreController().GetAll().ForEach(x => Console.WriteLine(x));
+    }
+}
